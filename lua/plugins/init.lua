231a8f65a72e5a402bc441aa10ce85f5c5829547
Francisco Pina Martins <stunts@pinamartins.com>
2024-08-05 23:25:31 +0100

    Updates the default config to add Biosyntax, Treesitter, LSPs, and Snakemake

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,14 +12,44 @@ return {
       require "configs.lspconfig"
     end,
   },
+  -- Added by Francisco
+  -- Do not override nvim-qt runtime path
+  {
+    "equalsraf/neovim-gui-shim",
+    enabled = true,
+  },
 
+  -- Snakemake syntax highlighting
+  -- Obtained from here:
+  -- https://github.com/folke/lazy.nvim/issues/183#issuecomment-1376469054
+  {
+    "snakemake/snakemake",
+    ft = "snakemake",
+    config = function(plugin)
+      vim.opt.rtp:append(plugin.dir .. "/misc/vim")
+      require("lazy.core.loader").packadd(plugin.dir .. "/misc/vim")
+    end,
+    init = function(plugin)
+      require("lazy.core.loader").ftdetect(plugin.dir .. "/misc/vim")
+    end,
+  },
+
+  { "bioSyntax/bioSyntax-vim", lazy = false },
+
+  -- Make nvim reactive to window changes, buffer type, etc...
+  { 'rasulomaroff/reactive.nvim', lazy=false },
+  -- To make a plugin not be loaded
   -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
+  --   "NvChad/nvim-colorizer.lua",
+  --   enabled = false
   -- },
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
 }
